cd36b3debddab2285581a64d544ca6550e33f2c2
locustcode <124188098+locustcode@users.noreply.github.com>
2023-04-27 15:13:05 -0400

    Added Hash functions for table display, changed variable names for forms

--- front-end/scripts/items.js
+++ front-end/scripts/items.js
@@ -20,16 +20,59 @@ async function fetchData(selection){
     });
 
 }
-
-async function gatherData(selection){
+  
+  async function gatherData(selection) {
     let requiredTables = []
     let results = []
 
-    document.getElementsByClassName("header-title")[0].innerHTML = selection;
+
+    //Similar to the items and attributes in the form object map, this object maps keys to title names, and makes them the header of the table page 
+    const tableName = {
+        MSE: 'Mouses',
+        KeyB: 'Keyboards',
+        PowerSupply: 'Power Supply',
+        USB: 'USB',
+        Ethernet: 'Ethernet Cables',
+        Visual: 'Visual Cables',
+        Switches: 'Switches',
+        FireWall: 'Firewalls',
+        Audio: 'Audio Cables',
+        Other: 'Other',
+    };
+
+    document.getElementsByClassName("header-title")[0].innerHTML = tableName[selection];
+
     // document.getElementById("content").innerHTML = selection;
     switch(selection){
+
+        case "MSE":
+        requiredTables = ["MSE"];
+        results.push(["brand", "quantity"]);
+        for (var i = 0; i < requiredTables.length; i++) {
+            const rr = await fetchData(requiredTables[i])
+            for (var y = 0; y < rr.length; y++){
+                results.push(rr[y]);
+                // results.push(`There are ${rr[y]['quantity']} ${rr[y]['brand']} ${requiredTables[i]} available for checkout`)
+            }
+        }
+
+        break;
+
+        case "KeyB": 
+        requiredTables = ["KeyB"];
+        results.push(["brand", "quantity"]);
+        for (var i = 0; i < requiredTables.length; i++) {
+            const rr = await fetchData(requiredTables[i])
+            for (var y = 0; y < rr.length; y++){
+                results.push(rr[y]);
+                // results.push(`There are ${rr[y]['quantity']} ${rr[y]['brand']} ${requiredTables[i]} available for checkout`)
+            }
+        }
+
+        break;
+            
         
-        case "Cables":
+        case "Ethernet":
             requiredTables = ["Ethernet"];
             results.push(["Is Long?", "quantity"]);
             for (var i = 0; i < requiredTables.length; i++) {
@@ -40,7 +83,9 @@ async function gatherData(selection){
                 }
             }
 
+            break; 
 
+            case "USB":
             requiredTables = ["USB"];
             results.push(["Name", "connector", "quantity"]);
             for (var i = 0; i < requiredTables.length; i++) {
@@ -53,8 +98,8 @@ async function gatherData(selection){
             
             break; 
         
-        case "Hardware":
-            requiredTables = ["FireWall", "Switches"];
+        case "FireWall":
+            requiredTables = ["FireWall"];
             results.push(["brand", "name", "quantity"]);
             for (var i = 0; i < requiredTables.length; i++) {
                 const rr = await fetchData(requiredTables[i])
@@ -64,6 +109,22 @@ async function gatherData(selection){
                 }
             }
 
+            break; 
+
+            case "Switches":
+                requiredTables = ["Switches"];
+                results.push(["brand", "name", "quantity"]);
+                for (var i = 0; i < requiredTables.length; i++) {
+                    const rr = await fetchData(requiredTables[i])
+                    for (var y = 0; y < rr.length; y++){
+                        results.push(rr[y]);
+                        // results.push((`There are ${rr[y]['quantity']} ${rr[y]['brand']} ${rr[y]['name']}'s ${requiredTables[i]} available for checkout`))
+                    }
+                }
+
+            break; 
+            
+            case "PowerSupply":
             requiredTables = ["PowerSupply"]; 
             results.push(["Name", "Device Type", "quantity"]);
             for (var i = 0; i < requiredTables.length; i++) {
@@ -74,20 +135,9 @@ async function gatherData(selection){
                 }
             }            
 
-
             break;
 
-        case "Peripherals":
-            requiredTables = ["MSE", "KeyB"];
-            results.push(["brand", "quantity"]);
-            for (var i = 0; i < requiredTables.length; i++) {
-                const rr = await fetchData(requiredTables[i])
-                for (var y = 0; y < rr.length; y++){
-                    results.push(rr[y]);
-                    // results.push(`There are ${rr[y]['quantity']} ${rr[y]['brand']} ${requiredTables[i]} available for checkout`)
-                }
-            }
-
+            case "Audio":
             requiredTables = ["Audio"]; 
             results.push(["Name", "Cable Type", "quantity"]);
             for (var i = 0; i < requiredTables.length; i++) {
@@ -98,6 +148,9 @@ async function gatherData(selection){
                 }
             }
 
+            break;
+
+            case "Visual":
             requiredTables = ["Visuals"]; 
             results.push(["Name", "Cable Type", "quantity"]);
             for (var i = 0; i < requiredTables.length; i++) {
@@ -125,15 +178,57 @@ async function gatherData(selection){
             break;
     }
 
-    drawResults(results);
 
+    drawResults(results);
 }
 
+//Onclick of dropdown selection, this function creates a new URL path for the selection in items.html, with a hash with the name of the table
+//This way it still executes the gatherData() function even on page reload
+function loadResults(selection) {
+    window.location.href = 'items.html#' + selection;
+}
+
+
+//This function actually handles the selection hash change. Referrer is used if the hash is loaded from an html that is not items.html
+function handleHashChange() {
+    const hash = window.location.hash.slice(1);
+    if (hash) {
+        gatherData(hash);
+    } else {
+        // Check if the page was loaded from a dropdown link on another page
+        const referer = document.referrer;
+        //referer misspelled for variable name so it does not actually use referrer from js
+        if (referer) {
+            const refererHash = new URL(referer).hash.slice(1);
+            if (refererHash) {
+                gatherData(refererHash);
+                // Update the URL hash to match the selected table
+                window.location.hash = refererHash;
+            }
+        }
+    }
+}
+
+
+// Event listener for hash changes
+window.addEventListener("hashchange", handleHashChange);
+
+// Load the correct table based on the URL hash when the page is first loaded
+window.addEventListener("DOMContentLoaded", handleHashChange);
+
+
 function drawResults(results){
+
+    // Wiping content clean if anything already exists
+    let target = document.getElementById("content") || document.getElementById("content-home") || document.getElementById("content-form");
     
-    // Wipping content clean if anything already exists
-    let target = document.getElementById("content");
+    if (target.id !== "content") {
+        // If target div id is not 'content', set it to 'content' for styling purposes for the tables
+        target.id = "content";
+    }
+
     target.innerText = "";
+
 
     let table_wrapper = document.createElement("div");
     table_wrapper.classList.add("item-select-wrapper");
@@ -176,27 +271,3 @@ function drawResults(results){
 
 }
 
-function checkout(action) {
-    // POST request to /checkout
-    const selectedCategory = document.getElementById('category').value;
-    const subcategories = categoryToSubcategories[selectedCategory] || [];
-    const selectedSubcategories = {};
-    
-    subcategories.forEach(subcategory => {
-        selectedSubcategories[subcategory.key] = document.getElementById(subcategory.key).value;
-    });
-    fetch('http://localhost:3000/api/checkout', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-       body: JSON.stringify({
-        "action": action,
-        "wheres":selectedSubcategories, 
-        "table": selectedCategory
-       })
-    })
-    .then(response => response.json())
-    .then(response => console.log(JSON.stringify(response)))
-    .catch(error => console.error(error))
-}
