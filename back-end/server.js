bc98c4f76ea2b79135e43b9c1cc5926a5c1bd2e6
tterBrett <104647076+tterBrett@users.noreply.github.com>
2023-04-30 21:40:10 -0400

    updated /api/tables/:table logic
    Removed 53 lines of code to print tables

--- back-end/server.js
+++ back-end/server.js
@@ -34,52 +34,50 @@ app.get("/",  (req, res) => {
 
 // example
 app.get("/api/tables/:table", (req, res) => {
-    console.log("Fetching table:", req.params.table);
-    
-    if(["MSE", "KeyB"].includes(req.params.table)){
-
-        connection.query(`
-            select distinct brand, count(unique_ID) as quantity
-            from Inventory.${req.params.table}
-            where not checked_out
-            group by brand;`,
-        (err, rows, fields) => {
-            res.json(rows)
-        });
-    }
-    else if(["USB"].includes(req.params.table)){
-
-        connection.query(`
-            select distinct name, connector, count(unique_ID) as quantity
-            from Inventory.${req.params.table}
-            where not checked_out
-            group by name, connector;`,
-        (err, rows, fields) => {
-            res.json(rows)
-        });
-    }
-
-    else if(["Audio"].includes(req.params.table)){
-       
-        connection.query(`
-            select distinct name, cable_type, count(unique_ID) as quantity
-            from Inventory.${req.params.table}
-            where not checked_out
-            group by name, cable_type;`,
+    let i = 1
+    selectClause = ""
+    groupByClause = "GROUP BY "
+    tables = {
+        "MSE": ["brand"],
+        "KeyB": ["brand"],
+        "USB": ["name", "connector"],
+        "Audio": ["name", "cable_type"],
+        "PowerSupply": ["name", "device_type"],
+        "Visuals": ["name", "cable_type"],
+        "Switches": ["brand", "name"],
+        "FireWall":["brand", "name"],
         
-        (err, rows, fields) => {
+    }
+    query = "SELECT distinct "
+    if(Object.keys(tables).includes(req.params.table)){
+        for (const x of (tables[`${req.params.table}`])) {
+            if(i == 1){
+                selectClause += x
+                groupByClause += x
+            }else{
+                selectClause += "," + x
+                groupByClause += "," + x
+            }
+            i++;
+        }
+        selectClause += ", count(unique_ID) as quantity"
+        sql = query + selectClause + ` FROM inventory.${req.params.table} WHERE not checked_out ${groupByClause};`
+
+        connection.query(`${sql}`, (err, rows, fields) => {
+            console.log(err)
             res.json(rows)
         });
     }
-
     else if(["Ethernet"].includes(req.params.table)){
        
         connection.query(`
         select distinct 
 		    CASE 
-		    WHEN is_Long = 0 THEN 'Short'
-            ELSE 'Long' 
-	        END AS CableLength,
+             WHEN
+                 is_Long = 0 THEN 'Short'
+             ELSE 
+                 'Long' 
+	     END AS CableLength,
 		    count(unique_ID) as quantity
         from Inventory.${req.params.table}
         where not checked_out
@@ -89,61 +87,8 @@ app.get("/api/tables/:table", (req, res) => {
             res.json(rows)
         });
     }
-
-    else if(["PowerSupply"].includes(req.params.table)){
-       
-        connection.query(`
-            select distinct name, device_type, count(unique_ID) as quantity
-            from Inventory.${req.params.table}
-            where not checked_out
-            group by name, device_type;`,
-        
-        (err, rows, fields) => {
-            res.json(rows)
-        });
-    }
-
-    else if(["Visuals"].includes(req.params.table)){
-       
-        connection.query(`
-            select distinct name, cable_type, count(unique_ID) as quantity
-            from Inventory.${req.params.table}
-            where not checked_out
-            group by name, cable_type;`,
-        
-        (err, rows, fields) => {
-            console.log(err);
-            res.json(rows)
-        });
-    }    
-
-    else if(["Transaction"].includes(req.params.table)){
-       
-        connection.query(`
-        select distinct 
-	        CASE 
-            WHEN flow = 0 THEN 'Checked In' 
-            ELSE 'Checked Out'
-            END AS Flow, 
-            timeStamp, name, itemID, tableOrigin    
-        from Inventory.${req.params.table}
-            group by name;`,
-        
-        (err, rows, fields) => {
-            res.json(rows)
-        });
-    }    
-
     else{
-        connection.query(`
-            select distinct brand, name, count(unique_ID) as quantity
-            from Inventory.${req.params.table}
-            where not checked_out
-            group by brand, name;`,
-        (err, rows, fields) => {
-            console.log(err);
-            res.json(rows)
-        });
+        res.send("Table does not exist");
     }
     
     // res.send(`Loading table ${req.params.table}`);
@@ -159,6 +104,14 @@ app.post('/api/checkout', async (req, res) => {
     let whereClause = ""
 
     let action = true ? req.body.action == 'in' : false
+    console.log("Wheres: ", req.body.wheres)
+    if(req.body.table == 'Ethernet'){
+        if(req.body.wheres.is_Long == 'Long'){
+            req.body.wheres.is_Long = 1;
+        }else{
+            req.body.wheres.is_Long = 0;
+        }
+    }
 
     for (const [key, value] of Object.entries(req.body.wheres)) {
         if(i == 1){
